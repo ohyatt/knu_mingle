706e11af6a058d6e417550e25630ea99ff3c74cb
young-in <hardworkingini@gmail.com>
2024-09-21 04:08:20 +0900

    feat : JWT token 설정

---
+++ src/main/java/com/example/knu_mingle/config/WebConfig.java
@@ -0,0 +1,18 @@
+package com.example.knu_mingle.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**") // 모든 경로에 대해
+                .allowedOrigins("*") // 모든 출처 허용
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS"); // 허용할 HTTP 메서드
+        //.allowedHeaders("*") // 허용할 헤더
+        //.allowCredentials(true); // 쿠키 전송 허용 여부
+    }
+}
---
+++ src/main/java/com/example/knu_mingle/constants/Constants.java
@@ -0,0 +1,8 @@
+package com.example.knu_mingle.constants;
+
+public class Constants {
+    public static final String SECRET_KEY = "Yn2kjibddFAWtnPJ2AFlL8WXmohJMCvigQggaEypa5E=";
+    public static final long ONE_DAY_MILLIS = 86400000;
+    public static final String AUTHENTICATE_HEADER = "Authenticate";
+    public static final String BEARER = "Bearer";
+}
