47fddb27a14cbd38709470532cff53f2aa88c891
MINSU LEE <minsoo030232@gmail.com>
2024-09-21 00:46:04 +0900

    add: Keyword 추가

--- src/main/java/com/example/knu_mingle/controller/ReviewRestController.java
+++ src/main/java/com/example/knu_mingle/controller/ReviewRestController.java
@@ -3,25 +3,22 @@ package com.example.knu_mingle.controller;
 
 import com.example.knu_mingle.domain.User;
 import com.example.knu_mingle.repository.PostRepository;
+import com.example.knu_mingle.repository.ReviewRepository;
 import com.example.knu_mingle.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
-@org.springframework.web.bind.annotation.RestController
-@RequestMapping("/auth")
+@RestController
+@RequestMapping("/review")
 public class ReviewRestController {
 
-    @Autowired
-    PostRepository postrepository;
-    UserRepository userrepository;
+    @Autowired ReviewRepository reviewRepository;
+    @Autowired UserRepository userRepository;
 
-
-
-
+    @GetMapping
+    public
 
     @PostMapping("/register")
     public ResponseEntity<String> registerUser(@RequestBody User user) {
