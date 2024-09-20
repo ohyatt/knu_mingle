f8f49e6b1741c0d5d0fa3ba3d3218c3f3226b426
young-in <hardworkingini@gmail.com>
2024-09-21 01:28:27 +0900

    feat : ios google maps 구현 완료

--- hackathon2024-FRONTEND/knumingle/ios/Runner/AppDelegate.swift
+++ hackathon2024-FRONTEND/knumingle/ios/Runner/AppDelegate.swift
@@ -1,12 +1,14 @@
 import Flutter
 import UIKit
+import GoogleMaps
 
-@UIApplicationMain
+@main
 @objc class AppDelegate: FlutterAppDelegate {
   override func application(
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    GMSServices.provideAPIKey("AIzaSyACLyOGyIJk7R3igTbvX21-ZkLUbkRdRSA")
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
