55597b909e6b3f2f5734343240bb01657a2a7455
young-in <hardworkingini@gmail.com>
2024-09-21 04:30:52 +0900

    feat : secret 값 설정

--- src/main/java/com/example/knu_mingle/KnuMingleApplication.java
+++ src/main/java/com/example/knu_mingle/KnuMingleApplication.java
@@ -2,12 +2,16 @@ package com.example.knu_mingle;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class KnuMingleApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(KnuMingleApplication.class, args);
+		new SpringApplicationBuilder(KnuMingleApplication.class)
+				.properties("spring.config.location=classpath:/application.yml,classpath:/application-secret.yml")
+				.run(args);
 	}
-
 }
