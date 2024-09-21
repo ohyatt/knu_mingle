5a3744c42c358acef2b71a6a217c59a95b7c5792
MINSU LEE <minsoo030232@gmail.com>
2024-09-21 14:28:20 +0900

    ReviewRequestDto 수정

--- src/main/java/com/example/knu_mingle/dto/ReviewRequestDto.java
+++ src/main/java/com/example/knu_mingle/dto/ReviewRequestDto.java
@@ -1,4 +1,10 @@
 package com.example.knu_mingle.dto;
 
+import com.example.knu_mingle.domain.Enum.Keyword;
+
 public class ReviewRequestDto {
+    private Keyword keyword;
+    private String title;
+    private String content;
+    private String reaction;
 }
