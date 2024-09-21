fd2f876a28d6c02ce4b8e939a9234362ebf10827
MINSU LEE <minsoo030232@gmail.com>
2024-09-21 14:45:15 +0900

    ReviewRestController 수정

--- src/main/java/com/example/knu_mingle/service/ReviewService.java
+++ src/main/java/com/example/knu_mingle/service/ReviewService.java
@@ -4,12 +4,14 @@ import com.example.knu_mingle.domain.Keyword;
 import com.example.knu_mingle.domain.Market;
 import com.example.knu_mingle.domain.Review;
 import com.example.knu_mingle.domain.User;
+import com.example.knu_mingle.dto.MarketRequestDto;
 import com.example.knu_mingle.dto.ReviewRequestDto;
 import com.example.knu_mingle.repository.ReviewRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.NoSuchElementException;
 
 @Service
 public class ReviewService {
@@ -20,7 +22,7 @@ public class ReviewService {
 
     public String createReview(String accessToken, ReviewRequestDto requestDto) {
         String email = jwtService.getEmailFromToken(accessToken);
-        User user = userService.getUserByEmail(email).orElseThrow();
+        User user = userService.getUserByEmail(email);
 
         Review review = new Review();
         reviewRepository.save(review);
@@ -42,30 +44,19 @@ public class ReviewService {
         Review review = reviewRepository.findById(reviewId)
                 .orElseThrow(() -> new RuntimeException("Review not found"));
 
-        Review updatedReview = new Review(
-                review.getUser(), // 기존 사용자 정보 유지
-                Keyword.valueOf(requestDto.getKeyword().toUpperCase()),
-                requestDto.getTitle(),
-                requestDto.getContent(),
-                requestDto.getReaction()
-        );
-
-        reviewRepository.save(updatedReview);
+        reviewRepository.save(review);
         return "Success";
     }
 
     public String deleteReview(String accessToken, Long reviewId) {
         String email = jwtService.getEmailFromToken(accessToken);
-        User user = userService.getUserByEmail(email).orElseThrow(() -> new RuntimeException("User not found"));
+        User user = userService.getUserByEmail(email).
+                orElseThrow(() -> new RuntimeException("User not found"));
 
         Review review = reviewRepository.findById(reviewId)
                 .orElseThrow(() -> new RuntimeException("Review not found"));
 
-        if (!review.getUser().getId().equals(user.getId())) {
-            throw new RuntimeException("You are not authorized to delete this review");
-        }
-
         reviewRepository.delete(review);
-        return "Success";
+        return "Review Deleted";
     }
 }
