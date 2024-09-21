d6fd349f067fcea59c3e1ca8de13d028416c7f02
young-in <hardworkingini@gmail.com>
2024-09-21 16:34:21 +0900

    feat : market에 게시글 목록 불러오기, 게시글 상세 정보, 게시글 삭제 끝

--- src/main/java/com/example/knu_mingle/controller/MarketRestController.java
+++ src/main/java/com/example/knu_mingle/controller/MarketRestController.java
@@ -1,6 +1,8 @@
 package com.example.knu_mingle.controller;
 
 import com.example.knu_mingle.domain.Market;
+import com.example.knu_mingle.dto.MarketListResponseDto;
+import com.example.knu_mingle.dto.MarketPostResponseDto;
 import com.example.knu_mingle.dto.MarketRequestDto;
 import com.example.knu_mingle.dto.MarketUpdateDto;
 import com.example.knu_mingle.service.MarketService;
@@ -10,6 +12,8 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.parameters.P;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @org.springframework.web.bind.annotation.RestController
 @RequestMapping("/market")
@@ -24,9 +28,27 @@ public class MarketRestController {
         return ResponseEntity.status(201).body(marketService.createMarket(accessToken, market));
     }
 
+    @Operation(summary="특정 게시글 내용 확인")
+    @GetMapping("/{market_id}")
+    public ResponseEntity<MarketPostResponseDto> getMarket(@RequestHeader(HttpHeaders.AUTHORIZATION) String accessToken, @PathVariable Long market_id) {
+        return ResponseEntity.ok(marketService.getMarket(accessToken, market_id));
+    }
+
+    @Operation(summary="모든 게시글 조회")
+    @GetMapping()
+    public ResponseEntity<List<MarketListResponseDto>> getMarketList(@RequestHeader(HttpHeaders.AUTHORIZATION) String accessToken) {
+        return ResponseEntity.ok(marketService.getMarketList(accessToken));
+    }
+
     @Operation(summary = "중고거래 게시글 수정")
     @PutMapping("/{market_id}")
     public ResponseEntity<Object> updateMarket(@RequestHeader(HttpHeaders.AUTHORIZATION) String accessToken, @PathVariable Long market_id, @RequestBody MarketUpdateDto updateDto) {
         return ResponseEntity.ok(marketService.updateMarket(accessToken, market_id, updateDto));
+    }
+
+    @Operation(summary = "특정 게시글 삭제")
+    @DeleteMapping("/{market_id}")
+    public ResponseEntity<Object> deleteMarket(@RequestHeader(HttpHeaders.AUTHORIZATION) String accessToken, @PathVariable Long market_id) {
+        return ResponseEntity.ok(marketService.deleteMarket(accessToken, market_id));
     }
 }
